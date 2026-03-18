3f026ce085554b1ab2c251124d2cd2a2bfdca5bd
nbarinas <nbarinas@gmail.com>
2026-03-18 14:25:37 -0500

    fix: import Boolean in models

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from .database import Base, UsersBase
